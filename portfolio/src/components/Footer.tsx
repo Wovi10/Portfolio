e23cf69b424f84a472250b8d47e737df6dc9f37d
Wout Vinckevleugel <wout.vinck@gmail.com>
2022-06-19 22:44:58 +0200

    Last commit and then push of the day

--- portfolio/src/components/Footer.tsx
+++ portfolio/src/components/Footer.tsx
@@ -3,14 +3,13 @@ import Socials from "./Socials.tsx";
 
 export default function Footer() {
     return (
-        <footer className="w-screen text-center lg:text-left bg-black text-white absolute bottom-0">
-            <div className="mx-6 py-10 text-center">
-                <div className="">
-                    <h6 className="uppercase font-semibold mb-4 text-center md:justify-start">
-                        My socials
-                    </h6>
-                    <Socials/>
-                </div>
+        <footer className="w-screen bg-black text-white absolute h-auto py-5">
+            <hr/>
+            <div className="flex m-6 absolute right-0 -bottom-6">
+                <h6 className="uppercase font-semibold md:justify-start mr-4">
+                    My socials
+                </h6>
+                <Socials/>
             </div>
         </footer>
     );
--- portfolio/src/components/Socials.tsx
+++ portfolio/src/components/Socials.tsx
@@ -8,17 +8,16 @@ import {socials} from "../data/Socials.tsx";
 export default function Socials() {
     return (
         <section id="socials">
-            <div className="flex mx-auto align-content-center">
-                <div className="flex items-center justify-center md:justify-start mb-4 flex-wrap">
+            <div className="flex justify-content-center">
+                <div className="flex items-center justify-center md:justify-start flex-wrap">
                     {socials.map((social, index) => (
-                        <p className="w-1/2" key={index}>
+                        <p className="" key={index}>
                             <div key={social.name}>
                                 <a
                                     href={social.link}
-                                    className="rounded flex hover:underline font-thin text-sm md:text-md pr-2"
+                                    className="rounded flex font-thin text-md pr-2"
                                 >
                                     <i className={`${social.icon} mr-2`}></i>
-                                    <p className="flex-shrink-0 text-white">{social.name}</p>
                                 </a>
                             </div>
                         </p>
