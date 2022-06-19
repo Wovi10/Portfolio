6eb8feaaa8a47b608d16b53da48be69c52cdd512
Wout Vinckevleugel <wout.vinck@gmail.com>
2022-06-19 20:39:25 +0200

    Created Navbar component

---
+++ portfolio/src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import {Container} from "./Container";
+
+export default function NavBar(){
+    return(
+        <section id="navbar">
+            <Container className="container mx-auto flex flex-col">
+                <div className="flex mx-auto md:w-1/2">
+                    <div className="flex text-white rounded-xl rounded bg-black md:p-8 mx-2 p-4 flex-wrap">
+
+                    </div>
+                </div>
+            </Container>
+        </section>
+    )
+}
