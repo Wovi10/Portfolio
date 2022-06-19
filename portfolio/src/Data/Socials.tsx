2f58a4e518489b86cd32e3a57691005d8236db0a
Wout Vinckevleugel <wout.vinck@gmail.com>
2022-06-19 20:25:28 +0200

    Edited styles for Socials.tsx

--- portfolio/src/Data/Socials.tsx
+++ portfolio/src/Data/Socials.tsx
@@ -1,7 +1,32 @@
 export const socials = [
     {
-        name: "@Wovi10",
+        name: "Wovi10",
         link: "https://github.com/Wovi10",
         icon: "bi bi-github",
-    }
+    },
+    {
+        name: "wout_vinckevleugel",
+        link: "https://www.instagram.com/wout_vinckevleugel/",
+        icon: "bi bi-instagram",
+    },
+    {
+        name: "Wout Vinckevleugel",
+        link: "https://www.linkedin.com/in/wout-vinckevleugel/",
+        icon: "bi bi-linkedin",
+    },
+    {
+        name: "wout.vinck@gmail.com",
+        link: "mailto:wout.vinck@gmail.com",
+        icon: "bi bi-google",
+    },
+    {
+        name: "Wovi10#7909",
+        link: "",
+        icon: "bi bi-discord",
+    },
+    {
+        name: "Wout",
+        link: "https://open.spotify.com/user/p94xagpcl6wvivvongptutu5i?si=6251b6e07f924dae",
+        icon: "bi bi-spotify",
+    },
 ];
--- portfolio/src/components/Socials.tsx
+++ portfolio/src/components/Socials.tsx
@@ -2,10 +2,13 @@
 import {Container} from "./Container.tsx";
 // @ts-ignore
 import {socials} from "../Data/Socials.tsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 export default function Socials() {
     return (
-        <section id="skills">
+        <section id="socials">
             <Container className="container mx-auto flex flex-col">
                 <div className="flex mx-auto md:w-1/2">
                     <div className="flex text-white rounded-xl rounded bg-black md:p-8 mx-2 p-4 flex-wrap">
@@ -15,7 +18,6 @@ export default function Socials() {
                                     <a
                                         href={social.link}
                                         className="rounded flex py-2 hover:underline rounded font-thin text-sm md:text-md"
-                                        target={"_blank"}
                                     >
                                         <i className={`${social.icon} mx-4`}></i>
                                         <p className="flex-shrink-0 mr-4 text-white">{social.name}</p>
